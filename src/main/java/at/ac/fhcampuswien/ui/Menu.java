719539fd0230346b4b80c58014869020d0c94d5f
Kenohsi <91662939+Kenohsi@users.noreply.github.com>
2022-05-26 17:42:54 +0200

    Initial commit

---
+++ src/main/java/at/ac/fhcampuswien/ui/Menu.java
@@ -0,0 +1,100 @@
+package at.ac.fhcampuswien.ui;
+
+import at.ac.fhcampuswien.controllers.AppController;
+import at.ac.fhcampuswien.downloader.ParallelDownloader;
+import at.ac.fhcampuswien.downloader.SequentialDownloader;
+import at.ac.fhcampuswien.models.Article;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class Menu {
+    private static final String INVALID_INPUT_MESSAGE = "No valid input. Try again";
+    private static final String EXIT_MESSAGE = "Bye bye!";
+    private AppController controller;
+
+    public void start(){
+        String input;
+        controller = new AppController();
+
+        do{
+            System.out.println(getMenuText());
+            input = readLine();
+            handleInput(input);
+        } while(!input.equals("q"));
+
+    }
+
+    private void handleInput(String input){
+        switch (input) {
+            case "a" -> getTopHeadlinesAustria(controller);
+            case "b" -> getAllNewsBitcoin(controller);
+            case "y" -> getArticleCount(controller);
+            case "q" -> printExitMessage();
+            case "h" -> downloadURLs();
+            default -> printInvalidInputMessage();
+        }
+    }
+
+    // Method is needed for exercise 4 - ignore for exercise 2 solution
+    private void downloadURLs(){
+        int resultSequential = controller.downloadURLs(new SequentialDownloader());
+        // TODO print time in ms it took to download URLs sequentially
+
+        // TODO implement the process() function in ParallelDownloader class
+        int resultParallel = controller.downloadURLs(new ParallelDownloader());
+
+        // TODO print time in ms it took to download URLs parallel
+    }
+
+    private void getArticleCount(AppController controller) {
+        System.out.println("Number of articles: " + controller.getArticleCount());
+    }
+
+    private void getTopHeadlinesAustria(AppController controller) {
+        List<Article> articleList = controller.getTopHeadlinesAustria();
+
+        for( Article a : articleList) {
+            System.out.println(a);
+        }
+    }
+
+    private void getAllNewsBitcoin(AppController controller) {
+        System.out.println(controller.getAllNewsBitcoin());
+    }
+
+    public static void printExitMessage(){
+        System.out.println(EXIT_MESSAGE);
+    }
+
+    public static void printInvalidInputMessage(){
+        System.out.println(INVALID_INPUT_MESSAGE);
+    }
+    
+    private static String getMenuText(){
+        return """
+                *****************************
+                *   Welcome to NewsApp   *
+                *****************************
+                Enter what you wanna do:
+                a: Get top headlines austria
+                b: Get all news about bitcoin
+                y: Count articles
+                q: Quit program
+                c: Get provider with most articles
+                d: Get longest author name
+                e: Count articles from NY Times
+                f: Get articles with short title
+                g: Sort articles by content length
+                h: Download URLs
+                """;
+    }
+
+    private static String readLine() {
+        String value;
+        Scanner scanner = new Scanner(System.in);
+        value = scanner.nextLine();
+        return value.trim();
+    }
+
+}
